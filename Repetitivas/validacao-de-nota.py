dc334dfb47e12d89511f4939c5bce1c5bf7be5f6
Maria Clara <mariiaclara428@gmail.com>
2024-12-18 17:43:20 -0300

    correcao no exercicio

--- Repetitivas/validacao-de-nota.py
+++ Repetitivas/validacao-de-nota.py
@@ -2,13 +2,15 @@ while True:
     nota1 = float(input("Digite a primeira nota: "))
     if (nota1 >= 0) and (nota1 <= 10):
         break
-    print("Valor Inválido! Tente novamente.")
+    else:
+        print("Valor Inválido! Tente novamente.")
 
 while True:
     nota2 = float(input("Digite a segunda nota: "))
     if (nota2 >= 0) and (nota2 <= 10):
         break
-    print("Valor Inválido! Tente novamente")
+    else:
+        print("Valor Inválido! Tente novamente")
 
 media = (nota1 + nota2) / 2
 print(f"MÉDIA = {media:.2f}")
